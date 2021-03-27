6484700bcb66c76a9aaf71da2e832d43aace962c
Arvin <Arvin@Aspire2>
2021-03-27 18:10:16 +1300

    Integrated AJAX and Mailer

--- js/validate.js
+++ js/validate.js
@@ -1,85 +1,90 @@
 $(function(){
 	$('#validate').on('click',function(e){
-		var  preguntas = $('#preguntas').val(),
-			name = $('#name').val(),
-			Address = $('#addres').val(),
-			phone = $('#phone').val(),
-			email = $('#email').val(),
-			file = $('#file').val(),
-			date = $('#date').val();
+		e.preventDefault();
+		var form_preguntas = $('#preguntas').val(),
+			form_name = $('#form_name').val(),
+			form_address = $('#form_address').val(),
+			form_phone = $('#form_phone').val(),
+			form_email = $('#form_email').val(),
+			form_file = $('#form_file').val(),
+			form_date = $('#form_date').val();
 			var regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-
-			var validar = 0;
+			t = "";
 			var tja = "";
-			if ($('#preguntas').val() == '') {
+			var validar = 0;
+			var tja1 = false;
+			var tja2 = false;
+			var tja8 = "";
+			
+			if ($('#preguntas').val() === '') {
 			    $('#preguntas').css('background-color', 'red');
 			        validar = validar+1;
 			}else{
 				$('#preguntas').css('background-color', '#eee');
 				
 			}
-			if ($('#file').val() == 0) {
-			    $('#file').css('background-color', 'red');
+			if (!$('#form_file').val()) {
+			    $('#form_file').css('background-color', 'red');
 			        validar = validar+1;
 			}else{
-				$('#file').css('background-color', '#eee');
+				$('#form_file').css('background-color', '#eee');
 				
 			}
-			if (name == '') {
-			    $('#name').css('background-color', 'red');
+			if (form_name === '') {
+			    $('#form_name').css('background-color', 'red');
 			        validar = validar+1;
 			}else{
-				$('#name').css('background-color', '#eee');
+				$('#form_name').css('background-color', '#eee');
 				
 			}
-			if (Address == '') {
-			    $('#addres').css('background-color', 'red');
+			if (form_address === '') {
+			    $('#form_address').css('background-color', 'red');
 			        validar = validar+1;
 			}else{
-				$('#addres').css('background-color', '#eee');
+				$('#sform_address').css('background-color', '#eee');
 				
 			}
-			if (email == '' || (!regex.test(email))) {
-			        $('#email').css('background-color', 'red');
+			if (form_email === '' || (!regex.test(form_email))) {
+			        $('#form_email').css('background-color', 'red');
 			        validar = validar+1;
 			}else{
-				$('#email').css('background-color', '#eee');
+				$('#form_email').css('background-color', '#eee');
 				
 			}
-			t = "";
-			if (phone == '') {
-			        $('#phone').css('background-color', 'red');
+
+			if (form_phone === '') {
+			        $('#form_phone').css('background-color', 'red');
 			        validar = validar+1;
 			}else{
-				$('#phone').css('background-color', '#eee');
+				$('#form_phone').css('background-color', '#eee');
 				
 			}
-			if (date == '') {
-			        $('#date').css('background-color', 'red');
+			if (form_date === '') {
+			        $('#form_date').css('background-color', 'red');
 			        validar = validar+1;
 			}else{
-				$('#date').css('background-color', '#eee');
+				$('#form_date').css('background-color', '#eee');
 				
 			}
 			
-			var tja1 = false;
-			if ($("#q1").is(":checked") == true) {
+			
+			if ($("#q1").is(":checked") === true) {
 			    tja = "Presencial";
 			    tja1 = true;
 			}
-			if ($("#q2").is(":checked") == true) {
+			if ($("#q2").is(":checked") === true) {
 			    tja = "Telefonica";
 			    tja1 = true;
 			}
-			if ($("#q3").is(":checked") == true) {
+			if ($("#q3").is(":checked") === true) {
 			    tja = "WhatsApp";
 			    tja1 = true;
 			}
-			if ($("#q4").is(":checked") == true) {
+			if ($("#q4").is(":checked") === true) {
 			    tja = "Skype";
 			    tja1 = true;
 			}
-			if ($("#q5").is(":checked") == true) {
+			if ($("#q5").is(":checked") === true) {
 			    tja = "Viber";
 			    tja1 = true;
 			}
@@ -91,21 +96,19 @@ $(function(){
 				$('#query').css('background-color', '#444343');
 			}
 
-			var tja2 = false;
-			var tja8 = "";
-			if ($("#o1").is(":checked") == true) {
+			if ($("#o1").is(":checked") === true) {
 			    tja8 = "Necesito ayuda de un asesor de inmigración con licencia para preparar una";
 			    tja2 = true;
 			}
-			if ($("#o2").is(":checked") == true) {
+			if ($("#o2").is(":checked") === true) {
 			    tja8 = "Yo mismo estoy preparando una solicitud de visa. Solo necesito respuestas a algunas preguntas.";
 			    tja2 = true;
 			}
-			if ($("#o3").is(":checked") == true) {
+			if ($("#o3").is(":checked") === true) {
 			    tja8 = "Mi solicitud de visa ya ha sido enviada. Necesito la ayuda de un asesor de inmigración con licencia para preparar una respuesta a una carta recibida de emigración Nueva Zelanda.";
 			    tja2 = true;
 			}
-			if ($("#o4").is(":checked") == true) {
+			if ($("#o4").is(":checked") === true) {
 			    tja8 = "Otro";
 			    tja2 = true;
 			}
@@ -115,24 +118,41 @@ $(function(){
 			}else{
 				$('#pgts').css('background-color', '#444343');
 			}
-			if (validar == 0) {
+			if (validar === 0) {
+				$("#preloader, .preloader").fadeIn();
+
+				var postValue =  `
+					name=${encodeURI(form_name)}
+					&address=${encodeURI(form_address)}
+					&email=${encodeURI(form_email)}
+					&phone=${encodeURI(form_phone)}
+					&date=${encodeURI(form_date)}
+					&type=${encodeURI(tja)}
+					&option=${encodeURI(tja8)}
+					&file=${encodeURI(form_file)}
+					&query=${encodeURI(form_preguntas)}
+				`.replace(/\s/g, '');
 
 				$.ajax({
-						type: 'POST',
-						url: 'php/save.php',
-						data:'name='+name+'&address='+Address+'&email='+email+'&phone='+phone+'&date='+date+'&type='+tja+'&option='+tja8+'&file='+file+'&query='+preguntas,
-						dataType:'json',	
-						encode  : true
-					})
-					.done(function(datos){
-						if (datos){
-							alert("Informacion almacenada en la base de datos ....");
-							
-							document.location.href = 'index.html'							
-						}
-					});
+					type: 'POST',
+					url: 'php/save.php',
+					data: postValue,
+					dataType: 'json',
+					success: function(result){
+						$("#preloader, .preloader").fadeOut();
+						alert(result.message);
+						document.location.href = 'index.html'
+					},
+					error: function(xhr, status, error) {
+						$("#preloader, .preloader").fadeOut();
+						var err = eval("(" + xhr.responseText + ")");
+  						alert(err.Message);
+					}
+				});
+
+				return false
 			}
 			
-		e.preventDefault();
+
 	});
 });
--- php/save.php
+++ php/save.php
@@ -1,10 +1,12 @@
 <?php
+	// error_reporting(0);
 	$conexion = mysqli_connect('localhost', 'root', '', 'conacus');
-	//Create a new PHPMailer instance
 	
-	 
-	//Configuracion servidor mail
-		
+	include 'class.smtp.php';
+	include 'class.phpmailer.php';
+	
+	$mail = new PHPMailer(true);
+
 	$name = $_POST['name'];
 	$address = $_POST['address'];
 	$email = $_POST['email'];
@@ -15,15 +17,78 @@
 	$file = $_POST['file'];
 	$query = $_POST['query'];
 	$datos = true;
+
+	$return_arr = array(
+		"message" => "Informacion almacenada en la base de datos ....",
+		"name" => $name,
+		"address" => $address,
+		"email" => $email,
+		"phone" => $phone,
+		"date" => $date,
+		"type" => $type,
+		"file" => $file,
+		"query" => $query,
+		"datos" => $datos
+	);
+
+
+	// Email Sender Credentials
+	$emailFromName = "Kiwi Wide";
+	$emailFrom = "simeonxianodos@gmail.com";
+	$emailFromPass = "XianScimon23";
+
+	// Receiver
+	$emailToName = $name;
+	$emailTo = $email; // you can replace any email here
+
+	$message = json_encode($return_arr);
+
+	try {
+		$mail->SMTPDebug = 0;                               // SMTP::DEBUG_SERVER or 0
+		$mail->isSMTP();                                    //Send using SMTP
+		$mail->Host       = 'smtp.gmail.com';               //Set the SMTP server to send through
+		$mail->SMTPAuth   = true;                           //Enable SMTP authentication
+		$mail->Username   = $emailFrom;                     //SMTP username
+		$mail->Password   = $emailFromPass;                 //SMTP password
+		$mail->SMTPSecure = 'tls';    						//Enable TLS encryption;
+		$mail->Port       = 587;
+
+		$mail->setFrom($emailFrom, $emailFromName);  // Add Sender
+		$mail->addAddress($emailTo, $emailToName); // Add Receiver here
+
+		//Attachments
+		// $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+		// $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+	
+		//Content
+		$mail->isHTML(true);
+		$mail->Subject = 'You received message from '. $name;
+		$mail->Body    = $message; // can be HTML string here
+		$mail->AltBody = $message; // Text Only
+
+		$mail->send();
 		
-	mysqli_query($conexion,"INSERT INTO info (name,address,email,phone,datei,type,optionq,query,file) VALUES('".$name."','".$address."','".$email."','".$phone."','".$date."','".$type."','".$option."','".$query."','".$file."')");
+		echo json_encode($return_arr);
+	} catch (Exception $e) {
+
+		$return_arr = array(
+			"message" => "Error - Something went wrong...",
+			"Mailer Error" => "{$mail->ErrorInfo}",
+		);
+
+		echo json_encode($return_arr);
+	}
+
+	// old code
+	// mysqli_query($conexion,"INSERT INTO info (name,address,email,phone,datei,type,optionq,query,file) VALUES('".$name."','".$address."','".$email."','".$phone."','".$date."','".$type."','".$option."','".$query."','".$file."')");
 	
-	$mensaj = "name = "+$name+" address = "+$address+" email = "+$email+" phone = "+$phone+" date = "+$date+"type = "+$type+" option = "+$option+" file = "+$file+" query = "+$query;
-	mail($email,"Datos",$mensaj);
+	// $mensaj = "name = "+$name+" address = "+$address+" email = "+$email+" phone = "+$phone+" date = "+$date+"type = "+$type+" option = "+ $option +" query = "+$query;
+	// // $mensaj = "name = "+$name+" address = "+$address+" email = "+$email+" phone = "+$phone+" date = "+$date+"type = "+$type+" option = "+$option+" file = "+$file+" query = "+$query;
+	// mail($to,"Datos lalalal",$mensaj. $headers);
 	
-	echo"<script type='text/javascript'>
-	           alert('Correo Enviado Correctamente');
-	        </script>";	
-	echo json_encode($datos);
+	// echo"<script type='text/javascript'>
+	//            alert('Correo Enviado Correctamente');
+	//         </script>";	
+	// echo json_encode($datos);
 
 ?>
